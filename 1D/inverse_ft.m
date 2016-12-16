0c0cc194d2ba058d1461f0673e07b10d4a2b8618
Yuchen Yue <yue.physics@gmail.com>
2016-12-16 00:20:24 -0500

    ifft times n makes more sense

--- 1D/inverse_ft.m
+++ 1D/inverse_ft.m
@@ -3,5 +3,5 @@ function y = inverse_ft(phi,Nx,deltaf)
 lin_1 = linspace(0,Nx-1,Nx);
 parity = (-1).^lin_1;
 phi_1 =parity.*phi;
-y = ifft(phi_1).*deltaf;
+y = ifft(phi_1).*deltaf*Nx;
 y = y.* parity;
