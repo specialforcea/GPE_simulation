2cabce8a54a49341164fda2f3b97ee8d3f1b3620
specialforcea <specialforceatp@gmail.com>
2017-01-04 12:43:40 -0500

    not double counting interaction

--- 1D/ave_pot.m
+++ 1D/ave_pot.m
@@ -1,5 +1,5 @@
 function y = ave_pot(phi,Nx,DeltaX,X,V,k_scale,Beta)
-pot = (1/2*X.*X + Beta*sq(phi) + V.*sin(k_scale.*X).^2).*sq(phi);
+pot = (1/2*X.*X + 1/2*Beta*sq(phi) + V.*sin(k_scale.*X).^2).*sq(phi);
 tot = pot;
 tot_L = tot(1:Nx-1);
 tot_H = tot(2:Nx);
