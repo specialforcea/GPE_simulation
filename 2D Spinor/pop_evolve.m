d294e3e524b125211d9de11e895a4c96ca7e30c5
specialforcea <specialforceatp@gmail.com>
2017-02-09 17:49:56 -0500

    interaction didn't play

--- 2D Spinor/pop_evolve.m
+++ 2D Spinor/pop_evolve.m
@@ -1,4 +1,4 @@
-evo_time = 0.04;
+evo_time = 0.12;
 Deltat_count = 1e-4;
 Deltat = 1e-5;
 evoN = int16(evo_time/Deltat_count);
@@ -9,6 +9,10 @@ for i = 1:evoN
     population(1,i) = integr2d(sq(phi_evo(:,:,1)),Nx,Ny,deltax,deltay);
     population(2,i) = integr2d(sq(phi_evo(:,:,2)),Nx,Ny,deltax,deltay);
     population(3,i) = integr2d(sq(phi_evo(:,:,3)),Nx,Ny,deltax,deltay);
+    %population(1,i) = sq(phi_evo(100,100,1));
+   
 end
-popN = linspace(1e-4,1e-4*41,42);
-plot(popN,population(:,1:42)')
+
+
+% popN = linspace(1e-4,1e-4*399,400);
+% plot(popN,population(:,1:400)')
