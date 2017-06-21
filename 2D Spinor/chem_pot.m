95481c4eb600856714e19ac566ef052430d5a61a
specialforcea <specialforceatp@gmail.com>
2017-06-21 14:49:45 -0400

    Revert "groundstate and chempot"
    This reverts commit d7dc86c1bf55a734b495c96c4a0ef16eaec319d4.

--- 2D Spinor/chem_pot.m
+++ 2D Spinor/chem_pot.m
@@ -1,16 +1,15 @@
-function y = chem_pot(phi,X,Y,Nx,Ny,Beta,k_scale,deltax,deltay,deltafx,deltafy,V,Lx,Ly,paritx,parity,gamma)
-lin_nx =  (0:1:Nx-1) - (Nx-1)/2;
-lin_ny =  (0:1:Ny-1) - (Ny-1)/2;
-fourier_phi = fourier_transform2(phi,paritx(:,:,1),parity(:,:,1),deltax,deltay);
-d_fourier_phix = (2i*pi/Lx)*(ones(Ny,1)*lin_nx).*fourier_phi;
-d_fourier_phiy = (2i*pi/Ly)*(lin_ny'*ones(1,Nx)).*fourier_phi;
-dphix = inverse_ft2(d_fourier_phix,paritx(:,:,1),parity(:,:,1),deltafx,deltafy,Nx,Ny);
-dphiy = inverse_ft2(d_fourier_phiy,paritx(:,:,1),parity(:,:,1),deltafx,deltafy,Nx,Ny);
+function y = chem_pot(phi,X,Nx,Beta,k_scale,deltax,deltaf,V,L)
+lin_n =  (0:1:Nx-1) - (Nx-1)/2;
+fourier_phi = fourier_transform(phi,Nx,deltax);
+d_fourier_phi = 2i*pi/L*lin_n.*fourier_phi;
+dphi = inverse_ft(d_fourier_phi,Nx,deltaf);
 
-kin = sq(dphix)./2 + sq(dphiy)./2;
-pot = (1/2*X.*X + 1/2*gamma^2*Y.*Y + 1/2*Beta*sq(phi) + V.*sin(k_scale.*X).^2).*sq(phi);
+kin = sq(dphi)./2;
+pot = (1/2*X.*X + 1/2*Beta*sq(phi) + V.*sin(k_scale.*X).^2).*sq(phi);
 tot = kin + pot;
-
-y = integr2d(tot,Nx,Ny,deltax,deltay);
+tot_L = tot(1:Nx-1);
+tot_H = tot(2:Nx);
+tot_avg = (tot_H+tot_L)/2;
+y = sum(tot_avg)*deltax;
 end
 
