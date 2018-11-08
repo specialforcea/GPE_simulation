2c3a81f5ac18b5d1e0e2c5883465b94342a93e55
Yuchen Yue <yue.physics@gmail.com>
2018-11-08 11:02:55 -0500

    average psd in radial direction

--- speckle analysis and simulation 1D scalar/get_power_spectra_from_image.m
+++ speckle analysis and simulation 1D scalar/get_power_spectra_from_image.m
@@ -1,19 +1,52 @@
 li=[20,25,30];
-fspeckle = zeros(3,Ns);
+
+r = (1:1:300)*deltasf/k_R*4*pi;
+pow_r = zeros(3,300);
 for i=1:3
     speckle = imread(strcat('speckle bench test data/12/',num2str(li(i)),'mm.png'));
     speckle = double(speckle);
     speckle = speckle - repmat(mean(speckle,2),1,Ns);
     
     %fsp = fourier_transform(speckle',Ns,deltas)';
-    fsp = fftshift(fft(speckle,Ns,2),2);
-    sq_fsp = sq(fsp);
+    fsp = fftshift(fft2(speckle));
+    fp = sq(fsp);
+    %fp = fp./max(max(fp));
+    [maxr,maxc] = find(fp==max(max(fp)));
     
     
     
-    pow = mean(sq_fsp);
+    X_cord = repmat((1:1:1280),1024,1);
+    Y_cord = repmat((1:1:1024)',1,1280);
     
-    fspeckle(i,:) = pow;
+    X_cord = X_cord - mean(maxc);
+    Y_cord = Y_cord - mean(maxr);
+    
+    r_cord = sqrt(X_cord.^2 + Y_cord.^2);
+    for k=1:300
+        [cordr,cordc] = find(r_cord<=k & r_cord>(k-1));
+        num = size(cordr,1);
+        po = 0;
+        for j=1:num
+            po  = po + fp(cordr(j),cordc(j));
+        end
+        po = po/num;
+        pow_r(i,k) = po;
+    end
+        
+        
+    figure(i)
+    subplot(121)
+    plot(r(3:end),pow_r(i,3:end))
+    title('PDS')
+    xlabel('k/k_R in radial direction')
+    ylabel('power density')
+    subplot(122)
+    plot(r,log(pow_r(i,:)))
+    title('log PDS')
+    xlabel('k/k_R in radial direction')
+    ylabel('log power density')
+    
+    
 end
     
     
