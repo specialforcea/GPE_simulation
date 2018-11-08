6b4f33c010ec4ec0ad98e2a9961784566b41ef69
Yuchen Yue <yue.physics@gmail.com>
2018-11-08 14:40:21 -0500

    fit linear psd to data and save plots

--- speckle analysis and simulation 1D scalar/get_power_spectra_from_image.m
+++ speckle analysis and simulation 1D scalar/get_power_spectra_from_image.m
@@ -34,19 +34,26 @@ for i=1:3
     end
         
         
+%     figure(i)
+%     subplot(121)
+%     plot(r(3:end),pow_r(i,3:end))
+%     title('PDS')
+%     xlabel('k/k_R in radial direction')
+%     ylabel('power density')
+%     subplot(122)
+%     plot(r,log(pow_r(i,:)))
+%     title('log PDS')
+%     xlabel('k/k_R in radial direction')
+%     ylabel('log power density')
+    
     figure(i)
-    subplot(121)
-    plot(r(3:end),pow_r(i,3:end))
-    title('PDS')
+    f = fit(r(7:end)',pow_r(i,7:end)','a*max(1-x/b,0)','StartPoint',[max(pow_r(i,7:end)),0.15]);
+    coe = coeffvalues(f);
+    plot(f,r(7:end),pow_r(i,7:end))
+    title('fit linear PSD to data')
     xlabel('k/k_R in radial direction')
     ylabel('power density')
-    subplot(122)
-    plot(r,log(pow_r(i,:)))
-    title('log PDS')
-    xlabel('k/k_R in radial direction')
-    ylabel('log power density')
-    
-    
+    text(0.3,2e11,strcat('k_m_a_x = ',num2str(coe(2))))
 end
     
     
