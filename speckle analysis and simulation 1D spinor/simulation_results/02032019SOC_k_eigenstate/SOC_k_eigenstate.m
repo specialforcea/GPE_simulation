8d001f9f278a09d99410cc484557dddcd45e97a3
Yuchen Yue <yue.physics@gmail.com>
2019-02-04 17:56:39 -0500

    preparing k eigenstate

--- speckle analysis and simulation 1D spinor/simulation_results/02032019SOC_k_eigenstate/SOC_k_eigenstate.m
+++ speckle analysis and simulation 1D spinor/simulation_results/02032019SOC_k_eigenstate/SOC_k_eigenstate.m
@@ -1,21 +1,19 @@
 
-grads = 50;%zeros(1,1601);
-%grads = [10,20,30,40,50,60];
-et = [1800,2000,2200,2400,2600,2800];
+grad = zeros(1,1601);
+grads = [10,20,30,40,50,60];
 spin1 = zeros(6,320);
 
 width = zeros(6,320);
 pos = zeros(6,320);
 
 for j=1:6
-    grad = zeros(1,et(j)+1);
-    grad(1:et(j)/2+1) = linspace(0,grads,et(j)/2+1);
-    grad(et(j)/2+2:end) = linspace(grads,0,et(j)/2);
+    grad(1:801) = linspace(0,grads(j),801);
+    grad(802:end) = linspace(grads(j),0,800);
     draw = 0;
     phi = phi_e;
     
     
-    for i=1:et(j)
+    for i=1:1600
         phi_1 = dynamic(phi,0.001,1e-5,c0,c2,Nx,-grad(i+1)*X,1,0,k_scale,f,deltax,deltaf,L,1*detuning,xmin,xmax,k_R,detuning);
        % phi_1 = dynamic(phi,0.001,1e-5,c0,c2,Nx,0,1,0,k_scale,f,deltax,deltaf,L,4*detuning,xmin,xmax,k_R,detuning);
 
@@ -27,7 +25,7 @@ for j=1:6
     %         plot(f./k_R*2*pi,sq(fourier_transform(phi,Nx,deltax)));
              drawnow;
         end
-        save(strcat('simulation_results/02042019SOC_k_eigenstate/phi_1_25ms_318ms_grad_',num2str(j),'.mat'),'phi_1');
+        save(strcat('simulation_results/02032019SOC_k_eigenstate/phi_1_25ms_318ms_grad_',num2str(j),'.mat'),'phi_1');
     end
 
 
@@ -47,10 +45,10 @@ for j=1:6
             plot(X,sq(phi(1,:)),X,sq(phi(2,:)))
             drawnow;
         end
-            save(strcat('simulation_results/02042019SOC_k_eigenstate/spin125ms_25ms_318ms_grad_',num2str(j),'.mat'),'spin1');
+            save(strcat('simulation_results/02032019SOC_k_eigenstate/spin125ms_25ms_318ms_grad_',num2str(j),'.mat'),'spin1');
             
-            save(strcat('simulation_results/02042019SOC_k_eigenstate/width25ms_25ms_318ms_grad_',num2str(j),'.mat'),'width');
-            save(strcat('simulation_results/02042019SOC_k_eigenstate/pos25ms_25ms_318ms_grad_',num2str(j),'.mat'),'pos');
+            save(strcat('simulation_results/02032019SOC_k_eigenstate/width25ms_25ms_318ms_grad_',num2str(j),'.mat'),'width');
+            save(strcat('simulation_results/02032019SOC_k_eigenstate/pos25ms_25ms_318ms_grad_',num2str(j),'.mat'),'pos');
 
     end
 end
