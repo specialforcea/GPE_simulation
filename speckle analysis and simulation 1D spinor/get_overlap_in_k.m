263778ed7ac9520391e9fcaa730e8fd3029e93c7
specialforcea <specialforceatp@gmail.com>
2019-02-26 12:04:48 -0500

    add get overlap in k

---
+++ speckle analysis and simulation 1D spinor/get_overlap_in_k.m
@@ -0,0 +1,32 @@
+
+overlap = zeros(1,18);
+for mk=1:18
+    load(strcat(wavepath,num2str(dts(mk)),'d_',num2str(OmegaR),'E.mat'));
+    phi_mk = phi_1.*[exp(1i.*ks(mk).*k_R.*X);exp(1i.*ks(mk).*k_R.*X);exp(1i.*ks(mk).*k_R.*X)];
+    
+    ol = zeros(1,10);
+    for j=1:10
+        phiiup = phi_mk(1,:);
+        phiidown = phi_mk(2,:);
+        
+        phifup = reshape(final_phi(mk,j,1,:),[1,Nx]);
+        phifdown = reshape(final_phi(mk,j,2,:),[1,Nx]);
+        a = sqrt(integr(sq(phiiup),Nx,deltax));
+        b = sqrt(integr(sq(phiidown),Nx,deltax));
+        c = sqrt(integr(sq(phifup),Nx,deltax));
+        d = sqrt(integr(sq(phifdown),Nx,deltax));
+        
+        kiup = fourier_transform(phiiup,Nx,deltax);
+        kidown = fourier_transform(phiidown,Nx,deltax);
+        kfup = fourier_transform(phifup,Nx,deltax);
+        kfdown = fourier_transform(phifdown,Nx,deltax);
+        
+        Oup = 1-JSD(sq(kiup),sq(kfup),Nx,deltaf);
+        Odown = 1-JSD(sq(kidown),sq(kfdown),Nx,deltaf);
+        ol(j) = a^2*c^2*Oup + b^2*d^2*Odown + 2*a*b*c*d*sqrt(Oup*Odown);
+    end
+    overlap(mk) = mean(ol);
+end
+        
+        
+        
