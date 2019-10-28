01483e34f3588c9a261b0c388d915ba100b37391
specialforcea <specialforceatp@gmail.com>
2019-10-28 17:55:32 -0400

    running kick and evolve real

--- speckle analysis and simulation 1D spinor/single_particle_SOC_AL.m
+++ speckle analysis and simulation 1D spinor/single_particle_SOC_AL.m
@@ -1,5 +1,5 @@
 ks = (1.2:0.01:3.5);
-OmegaR = 4.0;
+OmegaR = 0.5;
 band = (-3.5:0.001:3.5);
 
 Deltaq = @(x)(4.*x);
@@ -18,7 +18,7 @@ b = @(x)(b_0(x)./sqrt(a_0^2 + b_0(x).^2));
 c = @(x)(c_0./sqrt(c_0^2 + d_0(x).^2));
 d = @(x)(d_0(x)./sqrt(c_0^2 + d_0(x).^2));
 
-rho = @(x)(( acos(min(1,x./6))- x./6.*sqrt(1-(min(1,x./6)).^2))*2/pi);
+rho = @(x)(2700*( acos(min(1,x./6))- x./6.*sqrt(1-(min(1,x./6)).^2))*2/pi);
 
 scatter_rate_to_up = zeros(1,size(ks,2));
 scatter_rate_to_down = zeros(1,size(ks,2));
@@ -45,7 +45,7 @@ for i=1:size(ks,2)
         if eng_dif ==0
             fac = 1;
         else
-            fac = sin(eng_dif*50)^2/eng_dif^2;
+            fac = sin(eng_dif*1)^2/eng_dif^2;
         end
         scatter_rate_to_down(i) = scatter_rate_to_down(i) + fac*(a(band(j))*a(ks(i))+ b(band(j))*b(ks(i)))^2*rho(abs(band(j)-ks(i)));
     end
@@ -57,7 +57,7 @@ for i=1:size(ks,2)
         if eng_dif ==0
             fac = 1;
         else
-            fac = sin(eng_dif*50)^2/eng_dif^2;
+            fac = sin(eng_dif*1)^2/eng_dif^2;
         end
         scatter_rate_to_up(i) = scatter_rate_to_up(i) + fac*(c(band(j))*a(ks(i))+ d(band(j))*b(ks(i)))^2*rho(abs(band(j)-ks(i)));
     end
@@ -67,13 +67,13 @@ end
 scatter_rate = scatter_rate_to_down + scatter_rate_to_up;
 scatter_rate = scatter_rate./max(scatter_rate);
 % figure(2)
-subplot(325)
+subplot(321)
 scatter(E_down(ks),scatter_rate)
 hold on
 plot(E_down(ks),scatter_rate)
 ylabel('normalized scatter rate')
 xlabel('E_0/E_R')
-subplot(326)
+subplot(322)
 plot(band,E_up(band),band,E_down(band))
 text(0,5,strcat('Omega=',num2str(OmegaR),'E_R'))
 ylabel('E/E_R')
